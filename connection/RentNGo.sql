1467457469482efd68d45fe87a214c97f427acf8
TharushiMadhushani <139611254+TharushiMadhushani@users.noreply.github.com>
2024-12-13 02:01:13 +0000

    updated

--- connection/RentNGo.sql
+++ connection/RentNGo.sql
@@ -1,3 +1,4 @@
 create database RentNGo;
 use RentNGo;
 create table users(userid INT primary key auto_increment,fullname VARCHAR(255),mail VARCHAR(255) UNIQUE,pwsd VARCHAR(255));
+insert into users values(1,"Tharushi Madhushani","tharushimadushani@gmail.com","abc123"), (2,"Piyal Sanjeewa","psanjeewa@gmail.com","abc124"),(3,"M.Priyadharshani","priyadharshain@gmail.com","abc125");
